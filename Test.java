cf68f7b05e93c26fd359d69a318540bba17fd7d2
dmitrykovkov <d.a.kovkov@gmail.com>
2024-07-18 21:28:32 +0500

    создал новую ветку и комичу измения в нее

--- Test.java
+++ Test.java
@@ -1,5 +1,6 @@
 public class Test {
     public static void main(String[] args) {
         System.out.println("Меняю класс тест. Вспоминаю гитgit ");
+        System.out.println("Создал новую ветку в гите, сейчас сделаю коммит в ней");
     }
 }
